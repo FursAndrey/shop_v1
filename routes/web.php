6049bc5b42de8804a15305cabd504185c1aeab1b
Furs Andrei <you@example.com>
2022-04-14 14:44:54 +0300

    add index.html

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/shop', function () {
+    return view('shop/index');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
